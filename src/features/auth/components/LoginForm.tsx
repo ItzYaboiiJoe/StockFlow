c80acd2012acb214e6d85c536ac480d4ab6f3e1b
Joseph Seoudy <110504568+ItzYaboiiJoe@users.noreply.github.com>
2026-09-17 00:42:15 -0400

    setup new pages for register and login

---
+++ src/features/auth/components/LoginForm.tsx
@@ -0,0 +1,5 @@
+const LoginForm = () => {
+  return <div>Login Form Here</div>;
+};
+
+export default LoginForm;
---
+++ src/features/auth/components/RegisterForm.tsx
@@ -0,0 +1,5 @@
+const RegisterForm = () => {
+  return <div>Register Form Here</div>;
+};
+
+export default RegisterForm;
