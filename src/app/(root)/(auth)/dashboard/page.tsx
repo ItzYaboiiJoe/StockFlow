12d94f22a4e59c9e3179d0ccfedb9e3287a7b2c7
Joseph Seoudy <110504568+ItzYaboiiJoe@users.noreply.github.com>
2026-09-19 02:28:07 -0400

    Created 2 new pages (Dashboard, Onboarding) and created their starter components

---
+++ src/app/(root)/(auth)/dashboard/page.tsx
@@ -0,0 +1,7 @@
+import Dashboard from "@/features/dashboard/components/Dashboard";
+
+const DashboardPage = () => {
+  return <Dashboard />;
+};
+
+export default DashboardPage;
---
+++ src/app/(root)/(auth)/onboarding/page.tsx
@@ -0,0 +1,7 @@
+import Onboarding from "@/features/onboarding/components/Onboarding";
+
+const OnboardingPage = () => {
+  return <Onboarding />;
+};
+
+export default OnboardingPage;
