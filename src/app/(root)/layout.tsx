cabf00f9579a1cdec325a60fa0cb0f402fc02319
Joseph Seoudy <110504568+ItzYaboiiJoe@users.noreply.github.com>
2026-09-19 23:38:29 -0400

    Removed unused layout file

--- src/app/(root)/layout.tsx
+++
@@ -1,9 +0,0 @@
-const RootLayout = ({
-  children,
-}: Readonly<{
-  children: React.ReactNode;
-}>) => {
-  return <div className="min-h-screen bg-zinc-100">{children}</div>;
-};
-
-export default RootLayout;
