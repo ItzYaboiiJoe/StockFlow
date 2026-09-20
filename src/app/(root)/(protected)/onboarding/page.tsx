0e5bf0067f2928eaa5aad47f63dac5384602b42c
Joseph Seoudy <110504568+ItzYaboiiJoe@users.noreply.github.com>
2026-09-20 01:07:10 -0400

    Added server side protection to onboarding page to prevent logged in users to routing there manually

--- src/app/(root)/(protected)/onboarding/page.tsx
+++ src/app/(root)/(protected)/onboarding/page.tsx
@@ -1,6 +1,25 @@
 import Onboarding from "@/features/onboarding/components/Onboarding";
+import { createSupabaseServerClient } from "@/lib/db/supabaseServer";
+import { redirect } from "next/navigation";
 
-const OnboardingPage = () => {
+const OnboardingPage = async () => {
+  const supabase = await createSupabaseServerClient();
+
+  const {
+    data: { user },
+  } = await supabase.auth.getUser();
+
+  const { count, error } = await supabase
+    .from("business_users")
+    .select("*", { count: "exact", head: true })
+    .eq("user_id", user!.id);
+
+  if (error) throw error;
+
+  if (count! > 0) {
+    redirect("/dashboard");
+  }
+
   return <Onboarding />;
 };
 
